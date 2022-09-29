84d3985df0625f3e370f5b73c7fb58552f8ca702
zzezzee <GitHub zzezzee@naver.com>
2022-09-29 10:36:46 +0900

    20220929-문자열내마음대로정렬하기

--- week06/문자열-내-마음대로-정렬하기/java/20220929/src/Solution.java
+++ week06/문자열-내-마음대로-정렬하기/java/20220929/src/Solution.java
@@ -1,70 +1,20 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-public class Solution {
+class Solution {
     public String[] solution(String[] strings, int n) {
-        String[] answer = {};
+        String[] answer = new String[strings.length];
+        List<String> list = new ArrayList<>();
+
+        for(int i = 0; i < strings.length; i++) {
+            list.add(strings[i].charAt(n) + strings[i]);
+        }
+
+        Collections.sort(list);
+
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i).substring(1);
+        }
+
         return answer;
-    }
-
-    public boolean sameWord(String[] strings, int n) {
-        for (int i = 0; i < strings.length; i += 1) {
-            for (int j = i + 1; j < strings.length; j += 1) {
-                char c = strings[i].charAt(n);
-                char comparingChar = strings[j].charAt(n);
-
-                if (c == comparingChar) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    public String[] switchDifferentWord(String[] strings, int n) {
-
-        for (int i = 0; i < strings.length; i += 1) {
-            for (int j = i + 1; j < strings.length; j += 1) {
-                char c = strings[i].charAt(n);
-                char comparingChar = strings[j].charAt(n);
-
-                if (c > comparingChar) {
-                    String temp = strings[i];
-                    strings[i] = strings[j];
-                    strings[j] = temp;
-                }
-            }
-        }
-
-        return strings;
-    }
-
-    public String[] switchSameWord(String[] strings, int n) {
-        for (int i = 0; i < strings.length; i += 1) {
-            for (int j = i + 1; j < strings.length; j += 1) {
-                char c = strings[i].charAt(n);
-                char comparingChar = strings[j].charAt(n);
-
-                if (c == comparingChar) {
-                    for (int k = 0; k < strings.length; k += 1) {
-                        if (strings[i].charAt(k) > strings[j].charAt(k)) {
-                            String temp = strings[i];
-                            strings[i] = strings[j];
-                            strings[j] = temp;
-                        }
-                    }
-                }
-
-                if (c > comparingChar) {
-                    String temp = strings[i];
-                    strings[i] = strings[j];
-                    strings[j] = temp;
-                }
-            }
-        }
-
-        return strings;
     }
 }
