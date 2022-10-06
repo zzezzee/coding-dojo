7b76d04301c3550a6ffa21c883fd505cd37ee8be
zzezzee <GitHub zzezzee@naver.com>
2022-10-06 09:22:14 +0900

    20221006-나머지가1이되는수찾기

--- week07/나머지가-1이-되는-수-찾기/java/20221006/src/Solution.java
+++ week07/나머지가-1이-되는-수-찾기/java/20221006/src/Solution.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Solution {
+import java.util.stream.IntStream;
+
+public class Solution {
+    public int solution(int n) {
+        return IntStream.range(2, n)
+                .filter(i -> n % i == 1).findFirst().getAsInt();
+    }
 }
