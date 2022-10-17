f317e5829fafe799a0948c88aa176851c688f6c9
zzezzee <GitHub zzezzee@naver.com>
2022-10-17 13:52:48 +0900

    java

--- week09/자연수-뒤집어-배열로-만들기/java/20221017/src/Solution.java
+++ week09/자연수-뒤집어-배열로-만들기/java/20221017/src/Solution.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Solution {
+public class Solution {
+    public int[] solution(long n) {
+        return flip(longToString(n));
+    }
+
+    public String longToString(long n) {
+        String s = Long.toString(n);
+        return s;
+    }
+
+    public int[] flip(String s) {
+        int[] result = new int[s.length()];
+
+        for (int i = s.length() - 1; i >= 0; i -= 1) {
+            result[s.length() - i - 1] = s.charAt(i) - '0';
+        }
+
+        return result;
+    }
 }
--- week09/자연수-뒤집어-배열로-만들기/java/20221017/src/SolutionTest.java
+++ week09/자연수-뒤집어-배열로-만들기/java/20221017/src/SolutionTest.java
@@ -1,4 +1,27 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SolutionTest {
-  
+    @Test
+    void longToString() {
+        Solution solution = new Solution();
+
+        long n = 12345;
+
+        String s = solution.longToString(n);
+
+        assertEquals("12345", s);
+    }
+
+    @Test
+    void flip() {
+        Solution solution = new Solution();
+
+        String s = "12345";
+
+        int[] result = solution.flip(s);
+
+        assertEquals(5, result[0]);
+    }
 }
