d7219e732d4d65ea2d13739f8664c49e1e31796c
zzezzee <GitHub zzezzee@naver.com>
2022-10-21 11:11:20 +0900

    20221021-시저암호

---
+++ week09/시저-암호/javascript/solution.js
@@ -0,0 +1,4 @@
+function solution(s, n) {
+    var chars = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY                          "
+    return s.split('').map(e => chars[chars.indexOf(e)+n]).join('');
+}
