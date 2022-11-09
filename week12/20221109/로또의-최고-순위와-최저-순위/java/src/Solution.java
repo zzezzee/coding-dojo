b586ae13e5606ec5492e3c3fb46402b6b3790a03
zzezzee <GitHub zzezzee@naver.com>
2022-11-09 10:32:35 +0900

    스트림으로 풀기

--- week12/20221109/로또의-최고-순위와-최저-순위/java/src/Solution.java
+++ week12/20221109/로또의-최고-순위와-최저-순위/java/src/Solution.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int collectCount = getCollectCount(lottos, win_nums);
@@ -15,45 +20,40 @@ public class Solution {
     }
 
     public int getCollectCount(int[] lottos, int[] win_nums) {
-        int collectCount = 0;
+        List<Integer> win_nums_list = Arrays
+                .stream(win_nums)
+                .boxed()
+                .collect(Collectors.toList());
 
-        for (int i = 0; i < lottos.length; i++) {
-            for (int j = 0; j < win_nums.length; j++) {
-                if(lottos[i] == win_nums[j]){
-                    collectCount += 1;
-                }
-            }
-        }
-
-        return collectCount;
+        return IntStream.range(0, lottos.length).filter(i -> win_nums_list.contains(lottos[i])).toArray().length;
     }
 
     public int getZeroCount(int[] lottos) {
-        int zeroCount = 0;
-
-        for (int i = 0; i < lottos.length; i++) {
-            if(lottos[i] == 0){
-                zeroCount+= 1;
-            }
-        }
-
-        return zeroCount;
+        return Arrays.stream(lottos).filter(number -> number == 0).toArray().length;
     }
 
     public int[] getRanking(int[] count) {
-        int[] answer = new int[2];
-
-        for (int i = 0; i < count.length; i++) {
-            switch(count[i]){
-                case 6 -> answer[i] = 1;
-                case 5 -> answer[i] = 2;
-                case 4 -> answer[i] = 3;
-                case 3 -> answer[i] = 4;
-                case 2 -> answer[i] = 5;
-                default -> answer[i] = 6;
+        return Arrays.stream(count).map(i -> {
+            switch (i) {
+                case 6 -> {
+                    return 1;
+                }
+                case 5 -> {
+                    return 2;
+                }
+                case 4 -> {
+                    return 3;
+                }
+                case 3 -> {
+                    return 4;
+                }
+                case 2 -> {
+                    return 5;
+                }
+                default -> {
+                    return 6;
+                }
             }
-        }
-
-        return answer;
+        }).toArray();
     }
 }
